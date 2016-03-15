ab17300ad0f5a915b4b886f91817c169017e9139
Jimmy <hfter789@gmail.com>
2016-03-15 03:23:40 +0000

    done

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 var app = require('express')();
 var server = require('http').createServer(app);
+var MONTH = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var BASE_YEAR = 1900;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
+
+app.get('/:timestamp', function (req, res, next) {
+  console.log('timestamp is', req.params.timestamp);
+  var time = req.params.timestamp;
+  var timeInt = parseInt(time);
+  var timeString;
+
+  if(timeInt) {
+    // it is a number
+    var date = new Date(timeInt);
+    timeString = MONTH[date.getMonth()] + ' ' + date.getDate() + ', ' + (date.getYear() + BASE_YEAR);
+  } else {
+    timeString = time;
+    timeInt = new Date(timeString).getTime();
+  }
+  
+  if(!timeInt) {
+    timeString = null;
+  }
+  res.send({
+    unix: timeInt,
+    natural: timeString
+  });
+});
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
